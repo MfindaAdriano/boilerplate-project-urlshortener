84196e855cf42c9c6f6db33d101863164d1ebe9f
Mfinda Adriano <mfindaadriano@gmail.com>
2024-09-27 12:29:17 +0100

    first local commit

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+//use body parsing middleware
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -17,6 +21,18 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res) => {
+  let url = req.body.url;
+  
+  dns.lookup(url, 4, (error, family) => {
+    if(error) console.error("Mfinda, check the error: \n" + error);
+
+    //if everything is alright
+    console.log(url, family);
+  });
+  res.json({"original_url": url, "short_url": 1});
 });
 
 app.listen(port, function() {
