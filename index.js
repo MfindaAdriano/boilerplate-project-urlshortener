39f242f8d198aab8bc8ddb0c3b3b040280419fc3
Mfinda Adriano <mfindaadriano@gmail.com>
2024-09-30 18:00:22 +0100

    after cleaning the project code

--- index.js
+++ index.js
@@ -1,40 +1,122 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+//configure .env file
+require("dotenv").config();
+//importing modules
+const express = require("express");
+const validUrl = require("valid-url");
+const shortId = require("shortid");
+const mongoose = require("mongoose");
+
+let ShortenerModel = null;
+//Database handling
+try{
+    //connect to the database
+    mongoose.connect(process.env.DB_HOST);
+    console.log("DB connected successfully!");
+
+    //create a mongoose Schema
+    const ShortenerSchema = new mongoose.Schema({original_url: String, short_url: String});
+
+    //create Shortener Mode
+    ShortenerModel = mongoose.model("shortmodels", ShortenerSchema);
+
+}catch(error){
+    console.warn(`Failed to connect to the DB: ${error}`)
+}
+
+async function updateUrlData (data, model) {
+    const input = new model(data); 
+    let outputMessage;
+
+    await input.save()
+}
+//Database handling
+
+
+//function to validate the original url provided by the user
+function checkUrlValid(url){
+    //url = "http://localhost:3000";
+
+    const isHttps = validUrl.isHttpsUri(url);
+    const isHttp = validUrl.isHttpUri(url);
+
+    //if the provided url is a HTTP or HTTPS valid uri
+    if(typeof isHttps === "string" || typeof isHttp === "string"
+    ) return url;
+
+    //else return null;
+    return null;
+}
+
+
+
+//criando app and initiate it calling the listen method and using PORT defined in .env
 const app = express();
-const bodyParser = require('body-parser');
-const dns = require('dns');
+const port = process.env.PORT;
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`The Application is listen on port ${port}`));
 
-//use body parsing middleware
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(cors());
+//Some express configurations
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+    //res.send("You are In there");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post("/api/shorturl", async (req, res) => {
+    console.log(`url: ${req.url}\nBody: ${JSON.stringify(req.body)}`);
 
-app.post('/api/shorturl', (req, res) => {
-  let url = req.body.url;
-  
-  dns.lookup(url, 4, (error, family) => {
-    if(error) console.error("Mfinda, check the error: \n" + error);
+    const originalUrl = checkUrlValid(req.body.url);
+    
+    if(req.body.url !== ""){
+        //if the url is valid
+        //check if it is already on the DB
+        const urlInDB = await ShortenerModel.findOne({original_url: originalUrl});
 
-    //if everything is alright
-    console.log(url, family);
-  });
-  res.json({"original_url": url, "short_url": 1});
-});
+        //if url is already in the DB
+        if(urlInDB){
+            res.send({original_url: urlInDB.original_url, short_url: urlInDB.short_url});
+        }else{
+            if(typeof originalUrl === "string"){
+                const urlData = {original_url: originalUrl, short_url: shortId.generate()};
+        
+                //write to the DB
+                let responseDB;
+                updateUrlData(urlData, ShortenerModel)
+                    .then(() => console.log("DB Operation Status: Success" ))
+                    .catch((error) => console.log("DB Operation Status: Failure\n\t" + responseDB))
+    
+                
+                //render urlData object which contains the original and short url.
+                res.send(urlData);
+            }else{
+                res.send({error: "invalid url"});
+            }
+        }
+    }
+    
+    //res.send({urlData});
+}); //end of the post method to save shorten url
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.get("/api/shorturl/:short_url?", async (req, res) => {
+    const shortUrl = req.params.short_url;
+
+    //check if the shortUrl does exist in the DB
+    const shortInDB = await ShortenerModel.findOne({short_url: shortUrl});
+
+    //if it does exist
+    if(shortInDB){
+        console.log(shortInDB.original_url);
+
+        //redirect to the original URL found in the DB
+        res.redirect(shortInDB.original_url);
+        
+    }else console.log("Not in DB, I am sorry!");
+    
+})
+
+
+
+
+
